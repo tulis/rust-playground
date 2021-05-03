ebed0599cb81e7dc78520358dc39cd74a2f60b1e
tulis <tanri.suryati@outlook.com>
2021-05-03 23:21:12 +1000

    Learn about immutable and mutable variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    let immutable = 10;
+    println!("immutable is {}", immutable);
+
+    let mut mutable = 50;
+    println!("[Before] mutable is {}", mutable);
+    mutable = 60;
+    println!("[After] mutable is {}", mutable);
 }
